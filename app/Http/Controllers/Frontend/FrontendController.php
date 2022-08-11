511093cfd57d09e78de617e3195c5e2513c74625
arhamizza <arhamizza2@gmail.com>
2022-08-11 11:44:46 +0700

    view category

--- app/Http/Controllers/Frontend/FrontendController.php
+++ app/Http/Controllers/Frontend/FrontendController.php
@@ -11,8 +11,26 @@ class FrontendController extends Controller
 {
     public function index()
     {
-        $featured_products = Produk::where('trending','1')->take(15)->get();
-        $top_collection = Kategori::where('popular','1')->take(7)->get();
-        return view('index', compact('featured_products','top_collection'));
+        $featured_products = Produk::where('trending', '1')->take(15)->get();
+        $top_collection = Kategori::where('popular', '1')->take(7)->get();
+        return view('index', compact('featured_products', 'top_collection'));
+    }
+
+    public function category()
+    {
+        $category = Kategori::where('status', '0')->get();
+        return view('index', compact('category'));
+    }
+
+    public function viewcategory($slug)
+    {
+        if (Kategori::where('slug', $slug)->exists()) {
+            $kategori = Kategori::where('slug', $slug)->first();
+            $produk = Produk::where('cate_id', $kategori->id)->where('status', '0')->get();
+            return view('frontend.produk.index', compact('kategori','produk'));
+        } else {
+            return redirect('/')->where('status',"Slug doesnot exists");
+        }
+
     }
 }
