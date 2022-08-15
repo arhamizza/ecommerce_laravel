ded64840e1ef7471873f6e3c895c3c75f0b11728
muzakidian <muzakidian1@gmail.com>
2022-08-15 11:14:34 +0700

    cart

---
+++ app/Models/Cart.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    use HasFactory;
+    protected $table = 'carts';
+    protected $fillable =[
+        'user_id',
+        'prod_id',
+        'prod_qty',
+    ];
+}
---
+++ database/migrations/2022_08_15_040129_create_carts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('user_id');
+            $table->string('prod_id');
+            $table->string('prod_qty');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
